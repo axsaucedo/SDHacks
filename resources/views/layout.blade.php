924d5af13fb91c5bffbc4a4152c4b0ee07210135
Joseph Le <josephle@me.com>
2015-05-20 16:36:24 -0700

    Removed Angular scripts and added main javascript file to layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,6 +18,7 @@
 
 <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="{{ elixir('js/app.js') }}"></script>
 </body>
 
 
