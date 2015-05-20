69f78725adc66f325cd8084579bdec548be03213
Joseph Le <josephle@me.com>
2015-05-20 16:31:39 -0700

    Removed Angular dep

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,24 @@
 <html>
-    <body>
-        <h1>Laravel EE PUTOS</h1>
 
-        @yield('content')
-    </body>
+<head>
+    <title>SD Hacks</title>
+
+    <!--FONTS STYLES -->
+    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
+    <!--FONT AWESOME-->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}"/>
+</head>
+
+<body>
+@yield('content')
+
+<script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</body>
+
+
 </html>
