ff9e40d6e766d5e1e5c214c15d77421ed112ab13
Mike Shi <michaelshi42@gmail.com>
2015-06-13 17:29:46 -0700

    Fixing mobile scaling on index page, added new sections - Switching over the REMs and using media query for base font-size changing - Added added presented by section, added footer section - Fixed progress bar scaling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,10 +39,10 @@
                 5+ companies
             </div>
             <div class="bar">
-                36 hours
+                $1000s in prizes
             </div>
             <div class="bar">
-                $1000s in prizes
+                36 hours
             </div>
         </div>
     </section>
@@ -85,43 +85,62 @@
             <div class="question">
                 <h4>What If I Have a Question Not Answered Here?</h4>
                 <p>
-                    A hackathon is an event, typically lasting several days,in which a large number of people meet to
-                    engage in collaborative computer programming.
+                    Feel free to contact our team by sending an email to <a href="mailto:support@sdhacks.io" target="_blank">support@sdhacks.io</a>!
                 </p>
             </div>
         </div>
     </section>
     <section id="sponsors">
         <div id="sponsors-content" class="container-fluid">
-            <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="logo" style="background-image: url(http://d2vhierclfllnw.cloudfront.net/wp-content/uploads/sites/7/2014/09/affiliate-qualcomm-logo-01352357fc32cec6fb50356b86350d401.png);"></div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6">
                 <div class="logo" style="background-image: url(http://www.bloomberg.com/graphics/2015-cheatsheets/img/bofalogo.png)">
                 </div>
             </div>
-            <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="col-md-4 col-sm-6 col-xs-6">
                 <div class="logo" style="background-image: url(https://www.google.com/images/srpr/logo11w.png)">
                 </div>
             </div>
-            <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="col-md-4 col-sm-6 col-xs-4">
                 <div class="logo" style="background-image: url(http://www.govigaming.com/razer2.png)">
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-3 col-sm-6 col-xs-4">
                 <div class="logo" style="background-image: url(http://www.freelargeimages.com/wp-content/uploads/2014/11/Facebook_logo-9.png)">
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-3 col-sm-4 col-xs-4">
                 <div class="logo" style="background-image: url(http://i198.photobucket.com/albums/aa310/Govavi/GEAR%20STORE/VaviLogo.png)">
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-3 col-sm-4 col-xs-4">
                 <div class="logo" style="background-image: url(http://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Cisco_logo.svg/2000px-Cisco_logo.svg.png)">
                 </div>
             </div>
-            <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-3 col-sm-4 col-xs-4">
                 <div class="logo" style="background-image: url(http://upload.wikimedia.org/wikipedia/commons/5/5d/CBS_Interactive_Logo.PNG)">
                 </div>
             </div>
-
+        </div>
+    </section>
+    <section id="presented">
+        <div id="presented-content" class="container-fluid">
+            <div class="section-title col-xs-12">
+                Presented By
+            </div>
+            <div class="logo-container col-xs-12 col-md-8 col-md-offset-2">
+                <img alt="TESC | UC San Diego - Jacobs School of Engineering" src="/images/tesc_blue.png">
+                <img alt="MLH" src="http://static.mlh.io/brand-assets/logo/mlh-logo.png">
+            </div>
+        </div>
+    </section>
+    <section id="footer">
+        <div id="footer-content" class="container-fluid">
+            <img class="mini-logo" src="/images/mini_logo_white.png">
+            <a href="mailto:support@sdhacks.io" target="_blank">support@sdhacks.io</a>
+            <div style="float: right; padding-right: 15px;">Copyright SD Hacks 2015</div>
         </div>
     </section>
 </div>
