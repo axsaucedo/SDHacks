40edb43d75aa0bcaa2ca0ad1f2dd814213a12217
Joseph Le <josephle@me.com>
2015-06-14 11:45:58 -0700

    Added teams migration

---
+++ database/migrations/2015_06_14_184035_create_teams_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('teams', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('uuid')->unique();
+            $table->string('project');
+            $table->timestamps();
+        });
+
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->integer('team_id')->unsigned()->after('remember_token');
+            $table->foreign('team_id')->references('id')->on('teams');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::table('users', function(Blueprint $table) {
+            $table->dropForeign('users_team_id_foreign');
+        });
+
+		Schema::drop('teams');
+	}
+
+}
