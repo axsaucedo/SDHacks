34635feccb9395821cfe7a8ce984f12906cc1b07
Joseph Le <josephle@me.com>
2015-06-13 19:57:38 -0700

    Changed language of success page

--- resources/views/auth/success.blade.php
+++ resources/views/auth/success.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="panel panel-default">
-        <div class="panel-heading">Success</div>
+        <div class="panel-heading">Please Confirm Your Email</div>
         <div class="panel-body">
             <p>You should have received an email containing a confirmation link. Please verify your email address by
                 clicking the link before filling out your application.</p>
