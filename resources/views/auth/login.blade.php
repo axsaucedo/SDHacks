e11bdefa6cc0de85c94a3ff4427a6e8b51473db5
Joseph Le <josephle@me.com>
2015-06-13 19:34:06 -0700

    Abstracted layout from login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,61 @@
-@extends('layout')
+@extends('templates.one-sm-column')
 
 @section('content')
-    <div id="auth">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-4 col-md-offset-4">
-                    <div class="panel panel-default">
-                        <div class="panel-heading panel-logo text-center">
-                            <img src="{{ asset('/images/logo.png') }}" alt="SD Hacks Logo"/>
-                        </div>
-                        <div class="panel-body">
-                            @if (count($errors) > 0)
-                                <div class="alert alert-danger">
-                                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            @endif
-
-                            <form role="form" method="POST" action="{{ url('/auth/login') }}">
-                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-                                <div class="form-group">
-                                    <label class="control-label">Email</label>
-
-                                    <input type="email" class="form-control" name="email"
-                                           value="{{ old('email') }}">
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="control-label">Password</label>
-
-                                    <input type="password" class="form-control" name="password">
-                                </div>
-
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-
-
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <button type="submit" class="btn btn-primary btn-lg btn-block">Login</button>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <a href="{{ action('Auth\AuthController@getRegister') }}" class="btn btn-primary btn-lg
-                                        btn-block">Register</a>
-                                    </div>
-                                </div>
-
-                            </form>
-                        </div>
-                    </div>
-
-                    <div class="panel-extension text-center">
-                        <a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your
-                            Password?</a>
-                    </div>
-
+    <div class="panel panel-default">
+        <div class="panel-heading panel-logo text-center">
+            <img src="{{ asset('/images/logo.png') }}" alt="SD Hacks Logo"/>
+        </div>
+        <div class="panel-body">
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
                 </div>
-            </div>
+            @endif
+
+            <form role="form" method="POST" action="{{ url('/auth/login') }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <div class="form-group">
+                    <label class="control-label">Email</label>
+
+                    <input type="email" class="form-control" name="email"
+                           value="{{ old('email') }}">
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label">Password</label>
+
+                    <input type="password" class="form-control" name="password">
+                </div>
+
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="remember"> Remember Me
+                    </label>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <button type="submit" class="btn btn-primary btn-lg btn-block">Login</button>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="{{ action('Auth\AuthController@getRegister') }}" class="btn btn-primary btn-lg
+                                        btn-block">Register</a>
+                    </div>
+                </div>
+
+            </form>
         </div>
     </div>
 
+    <div class="panel-extension text-center">
+        <a class="btn-link" href="{{ url('/password/email') }}">Forgot Your
+            Password?</a>
+    </div>
 @endsection
