fe6e28a8cc34a4b2905691bba9c87e01f356554c
Joseph Le <josephle@me.com>
2015-06-16 00:37:01 -0400

    Fixed login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,66 +6,67 @@
 
 @section('form')
 
-<div class="col-xs-4 col-xs-offset-4">
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <form role="form" method="POST" action="{{ url('/auth/login') }}">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-        <div class="form-group">
-            <label class="control-label">Email</label>
-
-            <input type="email" class="form-control" name="email"
-                   value="{{ old('email') }}">
-        </div>
-
-        <div class="form-group">
-            <label class="control-label">Password</label>
-
-            <input type="password" class="form-control" name="password">
-        </div>
-
-        <div class="checkbox">
-            <label>
-                <input type="checkbox" name="remember"> Remember Me
-            </label>
-        </div>
-
-
-        <div class="row">
-            <div class="col-sm-6">
-                <button type="submit" class="btn btn-boxed-white btn-lg btn-block">Login</button>
+    <div class="col-xs-4 col-xs-offset-4">
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
-            <div class="col-sm-6">
-                <a href="{{ action('Auth\AuthController@getRegister') }}" class="btn btn-boxed-white btn-lg
-                                btn-block">Register</a>
+        @endif
+
+        <form role="form" method="POST" action="{{ url('/auth/login') }}">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+            <div class="form-group">
+                <label class="control-label">Email</label>
+
+                <input type="email" class="form-control" name="email"
+                       value="{{ old('email') }}">
             </div>
-        </div>
 
-                <br />
+            <div class="form-group">
+                <label class="control-label">Password</label>
 
-                <div class="row">
-                    <div class="col-xs-12">
-                        <a href="{{ action('Auth\AuthController@redirectToGitHub') }}" class="btn btn-boxed-white btn-lg btn-block">
-                            <i class="fa fa-lg fa-github"></i> Login/Register with GitHub</a>
-                    </div>
+                <input type="password" class="form-control" name="password">
+            </div>
+
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" name="remember"> Remember Me
+                </label>
+            </div>
+
+
+            <div class="row">
+                <div class="col-sm-6">
+                    <button type="submit" class="btn btn-boxed-white btn-lg btn-block">Login</button>
                 </div>
+                <div class="col-sm-6">
+                    <a href="{{ action('Auth\AuthController@getRegister') }}" class="btn btn-boxed-white btn-lg
+                                btn-block">Register</a>
+                </div>
+            </div>
 
-            </form>
+            <br/>
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <a href="{{ action('Auth\AuthController@redirectToGitHub') }}"
+                       class="btn btn-boxed-white btn-lg btn-block">
+                        <i class="fa fa-lg fa-github"></i> Login/Register with GitHub</a>
+                </div>
+            </div>
+
+        </form>
+
+        <div class="panel-extension text-center">
+            <a href="{{ url('/password/email') }}">Forgot Your
+                Password?</a>
         </div>
-    </div>
 
-    <div class="panel-extension text-center">
-        <a href="{{ url('/password/email') }}">Forgot Your
-            Password?</a>
     </div>
 @endsection
