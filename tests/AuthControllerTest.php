3a593f23742dedb87b5994747ec0a42218c94d34
Joseph Le <josephle@me.com>
2015-06-18 02:57:13 -0400

    Added basic registration tests

--- tests/AuthControllerTest.php
+++ tests/AuthControllerTest.php
@@ -1,8 +1,11 @@
 <?php
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Http\Controllers\Auth\AuthController;
 
 class AuthControllerTest extends TestCase {
+
+    use DatabaseTransactions;
 
     /**
      * Middleware
@@ -57,6 +60,117 @@ class AuthControllerTest extends TestCase {
         $this->action('GET', 'Auth\AuthController@getLogin');
 
         $this->see('Login');
+    }
+
+
+    /**
+     * Controller Tests
+     */
+
+    /**
+     * Registration
+     */
+
+    /**
+     * Test register link from login page
+     */
+    public function testRegisterLinkFromLogin()
+    {
+        $this->visit(action('Auth\AuthController@getLogin'));
+        $this->click('Register')
+            ->see('Register');
+    }
+
+    /**
+     * Test user registration form
+     */
+    public function testRegisterUserForm()
+    {
+        $this->visit(action('Auth\AuthController@getRegister'));
+        $this->type('test@example.org', 'email')
+            ->type('test1234', 'password')
+            ->type('test1234', 'password_confirmation')
+            ->press('Register');
+
+        // Check if success page shows
+        $this->see('Please confirm your email');
+
+        // Check if user has been persisted in database
+        $this->seeInDatabase('users', ['email' => 'test@example.org']);
+    }
+
+    /**
+     * Test user registration form without email
+     */
+    public function testRegisterUserFormWithoutEmail()
+    {
+        $this->visit(action('Auth\AuthController@getRegister'));
+        $this->type('test1234', 'password')
+            ->type('test1234', 'password_confirmation')
+            ->press('Register');
+
+        // Check if error message shows
+        $this->see('The email field is required.');
+    }
+
+    /**
+     * Test user registration form with duplicate email
+     */
+    public function testRegisterUserFormDuplicateEmail()
+    {
+        // Create user with email
+        $this->visit(action('Auth\AuthController@getRegister'));
+        $this->type('test@example.org', 'email')
+            ->type('test1234', 'password')
+            ->type('test1234', 'password_confirmation')
+            ->press('Register');
+
+        // Create user again
+        $this->visit(action('Auth\AuthController@getRegister'));
+        $this->type('test@example.org', 'email')
+            ->type('test1234', 'password')
+            ->type('test1234', 'password_confirmation')
+            ->press('Register');
+
+        // Check if error message shows
+        $this->see('Your email has already been taken!');
+    }
+
+    /**
+     * Test user registration form without password
+     */
+    public function testRegisterUserFormWithoutPassword()
+    {
+        $this->visit(action('Auth\AuthController@getRegister'));
+        $this->type('test@example.org', 'email')
+            ->press('Register');
+
+        // Check if error message shows
+        $this->see('The password field is required.');
+    }
+
+    /**
+     * Test user registration form without password
+     */
+    public function testRegisterUserFormUnconfirmedPassword()
+    {
+        $this->visit(action('Auth\AuthController@getRegister'));
+        $this->type('test@example.org', 'email')
+            ->type('test1234', 'password')
+            ->press('Register');
+
+        // Check if error message shows
+        $this->see('The password must be confirmed');
+
+
+        $this->visit(action('Auth\AuthController@getRegister'));
+        $this->type('test@example.org', 'email')
+            ->type('test1234', 'password')
+            ->type('test123', 'password_confirmation')
+            ->press('Register');
+
+        // Check if error message shows
+        $this->see('The password must be confirmed');
     }
 
 }
